12d2400a5eaea85589b70179ea7c7d2dc920a286
Alexander Leder <leder.alex05@gmail.com>
2023-09-29 12:27:55 +0200

    Settings 0.1

---
+++ Spiel/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spiel
+{
+    public partial class Settings : Form
+    {
+        
+
+        public Settings()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SettingsTonButton_MouseEnter(object sender, EventArgs e)
+        {
+            TrackBar SettingsTonSlider;
+            SettingsTonSlider = new TrackBar();
+            SettingsTonSlider.Visible = true;
+        }
+
+
+    }
+}
