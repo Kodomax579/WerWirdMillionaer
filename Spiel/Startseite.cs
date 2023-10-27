ac3e09603542ee30ec8930b8c1f99cafe8844c40
Kodomax579 <engelhardtcarsten00@gmail.com>
2023-10-27 22:52:23 +0200

    hgfd

---
+++ Spiel/Startseite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spiel
+{
+    public partial class Startseite : Form
+    {
+        public Startseite()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Settings set = new Settings();
+            set.Show();
+        }
+    }
+}
