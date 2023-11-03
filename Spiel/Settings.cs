daa37e731658c7cf63d20003535b64dd7f765f9e
Alexander Leder <leder.alex05@gmail.com>
2023-11-03 08:13:45 +0100

    w

--- Spiel/Settings.cs
+++ Spiel/Settings.cs
@@ -21,7 +21,7 @@ namespace Spiel
 
 
 
-        private TrackBar SettingsTonSlider = new TrackBar();
+        
 
         private void SettingsTonButton_MouseEnter(object sender, EventArgs e)
         {
@@ -42,6 +42,7 @@ namespace Spiel
             // Make the TrackBar control invisible
             SettingsTonSlider.Visible = false;
         }
+
 
 
 
